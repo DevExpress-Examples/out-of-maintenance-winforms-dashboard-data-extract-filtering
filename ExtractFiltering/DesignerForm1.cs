7b2f505eb0bb32f05bc13216c0eaa58b752dc01c
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-10-29 17:09:42 +0300

    add example

---
+++ ExtractFiltering/DesignerForm1.cs
@@ -0,0 +1,28 @@
+﻿
+using DevExpress.DashboardCommon;
+
+namespace ExtractFiltering
+{
+    public partial class DesignerForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public DesignerForm1()
+        {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"Dashboards\dashboard1.xml");
+
+            DashboardExtractDataSource dataExtract = new DashboardExtractDataSource();
+            dataExtract.ExtractSourceOptions.DataSource = dashboardDesigner.Dashboard.DataSources[0];
+            dataExtract.FileName = @"C:\Users\zakhodyaeva\Desktop\ExtractFiltering\ExtractFiltering\Data\Extract1.dat";
+            dataExtract.ExtractSourceOptions.Filter = "[CategoryName] = 'Beverages'";
+            dataExtract.Filter = "[ProductName] = 'Chai'";
+
+            Dashboard dashboard = new Dashboard();
+            dashboardDesigner.Dashboard = dashboard;
+            dashboardDesigner.Dashboard.DataSources.Add(dataExtract);
+            dashboardDesigner.Dashboard.SaveToXml(@"Dashboards\Dashboard_Extract.xml");
+            dataExtract.UpdateExtractFile();
+
+        }
+    }
+}
