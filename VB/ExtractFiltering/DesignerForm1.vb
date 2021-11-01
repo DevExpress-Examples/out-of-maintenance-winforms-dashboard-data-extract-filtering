1da782b38a57d6861d8e52936d1cb3dea1126d1e
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-11-01 14:06:51 +0300

    update Solutions

---
+++ VB/ExtractFiltering/DesignerForm1.vb
@@ -0,0 +1,27 @@
+﻿Imports DevExpress.DashboardCommon
+
+Namespace ExtractFiltering
+	Partial Public Class DesignerForm1
+		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner.CreateRibbon()
+			dashboardDesigner.LoadDashboard("Dashboards\dashboard1.xml")
+
+
+			Dim excelDataSource As New DashboardExcelDataSource() With {
+				.FileName = "C:\Users\zakhodyaeva\Desktop\winforms-dashboard-data-extract-filtering\CS\ExtractFiltering\Data\SalesPerson2.xlsx",
+				.SourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(New DevExpress.DataAccess.Excel.ExcelWorksheetSettings() With {.WorksheetName = "Sheet1"})
+			}
+
+			Dim dataExtract As New DashboardExtractDataSource()
+			dataExtract.ExtractSourceOptions.DataSource = excelDataSource
+			dataExtract.FileName = "C:\Users\zakhodyaeva\Desktop\winforms-dashboard-data-extract-filtering\CS\ExtractFiltering\Data\Extract1.dat"
+
+			dashboardDesigner.Dashboard.DataSources.Add(dataExtract)
+			dataExtract.UpdateExtractFile()
+
+		End Sub
+	End Class
+End Namespace
