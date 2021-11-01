63bda1c64aea00f6e83a54ce8be4cac3ce1d4e65
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-11-01 14:11:32 +0300

    add cs solution

---
+++ CS/ExtractFiltering/DesignerForm1.cs
@@ -0,0 +1,29 @@
+﻿
+using DevExpress.DashboardCommon;
+
+namespace ExtractFiltering {
+    public partial class DesignerForm1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public DesignerForm1() {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"Dashboards\dashboard1.xml");
+
+
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource() {
+                FileName = @"C:\Users\zakhodyaeva\Desktop\winforms-dashboard-data-extract-filtering\CS\ExtractFiltering\Data\SalesPerson2.xlsx",
+                SourceOptions = new DevExpress.DataAccess.Excel.ExcelSourceOptions (
+                new DevExpress.DataAccess.Excel.ExcelWorksheetSettings() {
+                    WorksheetName = "Sheet1",
+                })
+            };
+
+            DashboardExtractDataSource dataExtract = new DashboardExtractDataSource();
+            dataExtract.ExtractSourceOptions.DataSource = excelDataSource;
+            dataExtract.FileName = @"C:\Users\zakhodyaeva\Desktop\winforms-dashboard-data-extract-filtering\CS\ExtractFiltering\Data\Extract1.dat"; 
+
+            dashboardDesigner.Dashboard.DataSources.Add(dataExtract);
+            dataExtract.UpdateExtractFile();
+
+        }
+    }
+}
