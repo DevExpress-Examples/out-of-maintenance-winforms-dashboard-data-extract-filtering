fbc3494f85e041ae5b007a7665ea3d499ff8c509
DevExpressExampleBot <fakeEmail>
2021-12-15 12:44:05 +0300

    Source auto update [skip ci]

--- VB/ExtractFiltering/DesignerForm1.vb
+++ VB/ExtractFiltering/DesignerForm1.vb
@@ -8,21 +8,21 @@ Namespace ExtractFiltering
 			InitializeComponent()
 			dashboardDesigner.CreateRibbon()
 			dashboardDesigner.LoadDashboard("Dashboards\dashboard1.xml")
-			' Creates an origin Excel Data Source.
+			' Creates an origin Excel data source.
 			Dim excelDataSource As New DashboardExcelDataSource() With {
 				.FileName = "..\..\Data\SalesPerson2.xlsx",
 				.SourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(New DevExpress.DataAccess.Excel.ExcelWorksheetSettings() With {.WorksheetName = "Sheet1"})
 			}
-			' Creates a data extract based on the Excel Data Source.
+			' Creates a data extract based on the Excel data source.
 			Dim dataExtract As New DashboardExtractDataSource()
 			dataExtract.ExtractSourceOptions.DataSource = excelDataSource
 			dataExtract.FileName = "..\..\Data\Extract1.dat"
-			' Includes only "Beverages" rows from the "CategoryName" table to the Extract Data Source.
+			' Includes only "Beverages" rows from the "CategotyName" table to the Extract data source.
 			dataExtract.ExtractSourceOptions.Filter = "[CategoryName] = 'Beverages'"
 			' Limits displayed products from the "Beverages" category to "Chai".
 			dataExtract.Filter = "[ProductName] = 'Chai'"
 			dataExtract.UpdateExtractFile()
-			' Adds the Extract Data Source to the dashboard.
+			' Adds the Extract data souce to the dashboard. 
 			dashboardDesigner.Dashboard.DataSources.Add(dataExtract)
 		End Sub
 	End Class
